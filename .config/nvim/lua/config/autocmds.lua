b6efaa8bec0a9ec2aa088ca9ba31e9eb0e31a24d
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2025-06-12 09:37:54 +0530

    removed unwanted plugins

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 local api = vim.api
 
 -- don't auto comment new line
-api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+api.nvim_create_autocmd("BufEnter", {command = [[set formatoptions-=cro]]})
 
 -- wrap words "softly" (no carriage return) in mail buffer
 api.nvim_create_autocmd("Filetype", {
@@ -13,15 +13,12 @@ api.nvim_create_autocmd("Filetype", {
         vim.opt.linebreak = true
         vim.opt.columns = 80
         vim.opt.colorcolumn = "80"
-    end,
+    end
 })
 
 -- Highlight on yank
-api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-})
+api.nvim_create_autocmd("TextYankPost",
+                        {callback = function() vim.highlight.on_yank() end})
 
 -- go to last loc when opening a buffer
 -- this mean that when you open a file, you will be at the last position
@@ -32,37 +29,39 @@ api.nvim_create_autocmd("BufReadPost", {
         if mark[1] > 0 and mark[1] <= lcount then
             pcall(vim.api.nvim_win_set_cursor, 0, mark)
         end
-    end,
+    end
 })
 
 -- auto close brackets
 -- this
-api.nvim_create_autocmd("FileType", { pattern = "man", command = [[nnoremap <buffer><silent> q :quit<CR>]] })
+api.nvim_create_autocmd("FileType", {
+    pattern = "man",
+    command = [[nnoremap <buffer><silent> q :quit<CR>]]
+})
 
 -- show cursor line only in active window
-local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
-api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+local cursorGrp = api.nvim_create_augroup("CursorLine", {clear = true})
+api.nvim_create_autocmd({"InsertLeave", "WinEnter"}, {
     pattern = "*",
     command = "set cursorline",
-    group = cursorGrp,
+    group = cursorGrp
 })
-api.nvim_create_autocmd(
-    { "InsertEnter", "WinLeave" },
-    { pattern = "*", command = "set nocursorline", group = cursorGrp }
-)
+api.nvim_create_autocmd({"InsertEnter", "WinLeave"}, {
+    pattern = "*",
+    command = "set nocursorline",
+    group = cursorGrp
+})
 
 -- Enable spell checking for certain file types
-api.nvim_create_autocmd(
-    { "BufRead", "BufNewFile" },
-    -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
-    {
-        pattern = { "*.txt", "*.md", "*.tex" },
-        callback = function()
-            vim.opt.spell = true
-            vim.opt.spelllang = "en,de"
-        end,
-    }
-)
+api.nvim_create_autocmd({"BufRead", "BufNewFile"},
+-- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+                        {
+    pattern = {"*.txt", "*.md", "*.tex"},
+    callback = function()
+        vim.opt.spell = true
+        vim.opt.spelllang = "en,de"
+    end
+})
 
 -- vim.api.nvim_create_autocmd("ColorScheme", {
 --   callback = function()
@@ -74,26 +73,17 @@ api.nvim_create_autocmd(
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+    group = vim.api.nvim_create_augroup("close_with_q", {clear = true}),
     pattern = {
-        "PlenaryTestPopup",
-        "help",
-        "lspinfo",
-        "man",
-        "notify",
-        "qf",
-        "spectre_panel",
-        "startuptime",
-        "tsplayground",
-        "neotest-output",
-        "checkhealth",
-        "neotest-summary",
-        "neotest-output-panel",
+        "PlenaryTestPopup", "help", "lspinfo", "man", "notify", "qf",
+        "spectre_panel", "startuptime", "tsplayground", "neotest-output",
+        "checkhealth", "neotest-summary", "neotest-output-panel"
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
-        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-    end,
+        vim.keymap.set("n", "q", "<cmd>close<cr>",
+                       {buffer = event.buf, silent = true})
+    end
 })
 
 -- resize neovim split when terminal is resized
@@ -101,91 +91,114 @@ vim.api.nvim_command("autocmd VimResized * wincmd =")
 
 -- fix terraform and hcl comment string
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
-    callback = function(ev)
-        vim.bo[ev.buf].commentstring = "# %s"
-    end,
-    pattern = { "terraform", "hcl" },
+    group = vim.api.nvim_create_augroup("FixTerraformCommentString",
+                                        {clear = true}),
+    callback = function(ev) vim.bo[ev.buf].commentstring = "# %s" end,
+    pattern = {"terraform", "hcl"}
 })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+    group = vim.api.nvim_create_augroup('lsp-attach', {clear = true}),
     callback = function(event)
         local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+            vim.keymap.set("n", keys, func,
+                           {buffer = event.buf, desc = "LSP: " .. desc})
         end
-
 
         map("gl", vim.diagnostic.open_float, "Open Diagnostic Float")
         map("K", vim.lsp.buf.hover, "Hover Documentation")
         map("gs", vim.lsp.buf.signature_help, "Signature Documentation")
         map("gD", vim.lsp.buf.declaration, "Goto Declaration")
 
-        map("<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", "Goto Definition in Vertical Split")
+        map("<leader>v", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>",
+            "Goto Definition in Vertical Split")
 
         local wk = require("which-key")
         wk.add({
-            { "<leader>la", vim.lsp.buf.code_action,                           desc = "Code Action" },
-            { "<leader>lA", vim.lsp.buf.range_code_action,                     desc = "Range Code Actions" },
-            { "<leader>ls", vim.lsp.buf.signature_help,                        desc = "Display Signature Information" },
-            { "<leader>lr", vim.lsp.buf.rename,                                desc = "Rename all references" },
-            { "<leader>lf", vim.lsp.buf.format,                                desc = "Format" },
-            { "<leader>lc", require("config.utils").copyFilePathAndLineNumber, desc = "Copy File Path and Line Number" },
-            { "<leader>Wa", vim.lsp.buf.add_workspace_folder,                  desc = "Workspace Add Folder" },
-            { "<leader>Wr", vim.lsp.buf.remove_workspace_folder,               desc = "Workspace Remove Folder" },
+            {"<leader>la", vim.lsp.buf.code_action, desc = "Code Action"},
             {
+                "<leader>lA",
+                vim.lsp.buf.range_code_action,
+                desc = "Range Code Actions"
+            }, {
+                "<leader>ls",
+                vim.lsp.buf.signature_help,
+                desc = "Display Signature Information"
+            },
+            {"<leader>lr", vim.lsp.buf.rename, desc = "Rename all references"},
+            {"<leader>lf", vim.lsp.buf.format, desc = "Format"}, {
+                "<leader>lc",
+                require("config.utils").copyFilePathAndLineNumber,
+                desc = "Copy File Path and Line Number"
+            },
+            {
+                "<leader>Wa",
+                vim.lsp.buf.add_workspace_folder,
+                desc = "Workspace Add Folder"
+            }, {
+                "<leader>Wr",
+                vim.lsp.buf.remove_workspace_folder,
+                desc = "Workspace Remove Folder"
+            }, {
                 "<leader>Wl",
                 function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end,
-                desc = "Workspace List Folders",
-            },
+                desc = "Workspace List Folders"
+            }
         })
 
         local function client_supports_method(client, method, bufnr)
             if vim.fn.has 'nvim-0.11' == 1 then
                 return client:supports_method(method, bufnr)
             else
-                return client.supports_method(method, { bufnr = bufnr })
+                return client.supports_method(method, {bufnr = bufnr})
             end
         end
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
-            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        if client and client_supports_method(client, vim.lsp.protocol.Methods
+                                                 .textDocument_documentHighlight,
+                                             event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup(
+                                          'lsp-highlight', {clear = false})
+            vim.api.nvim_create_autocmd({'CursorHold', 'CursorHoldI'}, {
                 buffer = event.buf,
                 group = highlight_augroup,
-                callback = vim.lsp.buf.document_highlight,
+                callback = vim.lsp.buf.document_highlight
             })
 
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            vim.api.nvim_create_autocmd({'CursorMoved', 'CursorMovedI'}, {
                 buffer = event.buf,
                 group = highlight_augroup,
-                callback = vim.lsp.buf.clear_references,
+                callback = vim.lsp.buf.clear_references
             })
 
             vim.api.nvim_create_autocmd('LspDetach', {
-                group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+                group = vim.api
+                    .nvim_create_augroup('lsp-detach', {clear = true}),
                 callback = function(event2)
                     vim.lsp.buf.clear_references()
-                    vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
-                end,
+                    vim.api.nvim_clear_autocmds {
+                        group = 'lsp-highlight',
+                        buffer = event2.buf
+                    }
+                end
             })
         end
 
-
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+        if client and
+            client_supports_method(client, vim.lsp.protocol.Methods
+                                       .textDocument_inlayHint, event.buf) then
             map('<leader>th', function()
-                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+                vim.lsp.inlay_hint.enable(
+                    not vim.lsp.inlay_hint.is_enabled {bufnr = event.buf})
             end, '[T]oggle Inlay [H]ints')
         end
-    end,
+    end
 
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    callback = function()
-        pcall(vim.treesitter.start)
-    end,
+    callback = function() pcall(vim.treesitter.start) end
 })
